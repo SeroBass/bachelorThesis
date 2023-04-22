f5c6b51ef203c73b41fb432664b6a207629bfb5e
serobass <serhad1993@hotmail.com>
2023-04-23 01:36:14 +0200

    ml stuff

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import collect_data
 import graham_strategy
 import trader
 from ml import randomForestClassifier, ml_preprocessor
-from ml import decisionTreeClassifier
+from ml import decisionTreeClassifier, hyperParamTuning
 
 #collect_data.download_data()
 #graham_strategy.search_for_possibilities()
@@ -11,5 +11,6 @@ from ml import decisionTreeClassifier
 #ml_preprocessor.merge_all_companies()
 #ml_preprocessor.clean_table()
 #randomForestClassifier.rfc()
+hyperParamTuning.tune_dtc()
 #decisionTreeClassifier.dtc()
 #trader.trade_ml_50()
